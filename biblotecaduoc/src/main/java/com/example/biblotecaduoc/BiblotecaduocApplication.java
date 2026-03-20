9b25eb3f4dd80327c4de337984779275c50169c6
EL jona <jonat.garridos@duocuc.cl>
2026-03-20 13:49:23 -0300

    primer commit

---
+++ biblotecaduoc/src/main/java/com/example/biblotecaduoc/BiblotecaduocApplication.java
@@ -0,0 +1,13 @@
+package com.example.biblotecaduoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiblotecaduocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BiblotecaduocApplication.class, args);
+	}
+
+}
